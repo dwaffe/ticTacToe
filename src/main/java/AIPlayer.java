140a5052a872522b34451082d09419f52f03808e
dwaffe <dwaffe@gmail.com>
2019-07-14 21:31:33 +0200

    better AI

--- src/main/java/AIPlayer.java
+++ src/main/java/AIPlayer.java
@@ -13,7 +13,31 @@ public class AIPlayer extends Player {
         for (int i = 1; i <= 9; i++) {
             Board tempBoard = new Board(board);
             tempBoard.setField(i, getPiece());
+
+            if (!MoveValidator.validate(tempBoard, i)) {
+                continue;
+            }
             if ((new GameProgress(tempBoard)).isPieceWon(getPiece())) {
+                return i;
+            }
+        }
+        Piece opponent;
+
+        if (getPiece().getSymbol() == 'X') {
+            opponent = new OPiece();
+        } else {
+            opponent = new XPiece();
+        }
+
+        for (int i = 1; i <= 9; i++) {
+            Board tempBoard = new Board(board);
+
+            if (!MoveValidator.validate(tempBoard, i)) {
+                continue;
+            }
+
+            tempBoard.setField(i, opponent);
+            if ((new GameProgress(tempBoard)).isPieceWon(opponent)) {
                 return i;
             }
         }
